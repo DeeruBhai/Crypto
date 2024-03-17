cfeda3328cf4ff20d80749ef6f2c768c7aa1fc28
Deeraj Alagarsamy V R <118270248+DeeruBhai@users.noreply.github.com>
2024-03-17 19:17:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,14 @@ function App() {
           <h1 className="text-[#f4eff2] font-mono  text-4xl justify-center flex pt-4 uppercase ">
             <b>Track and trade</b>
           </h1>{" "}
-          <p className="text-[#FFD0EC] uppercase font-mono bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-8xl justify-center flex pt-2 ">
+          <p className="text-[#FFD0EC] uppercase font-mono bg-clip-text text-transparent text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-8xl justify-center flex pt-2 ">
             <b>Crypto currencies</b>
           </p>
         </div>
         <CoinModal />
       </section>
       <section>
-        <div className="text-[#FFD0EC] font-mono text-xl  items-center justify-center flex flex-row">
+        <div className="text-[#FFD0EC] font-mono text-xl  items-center justify-center flex flex-row relative md:mt-16 md:pt-16">
           <p className="w-3/4">
             A cryptocurrency, crypto-currency, or crypto is a digital currency
             designed to work as a medium of exchange through a computer network
